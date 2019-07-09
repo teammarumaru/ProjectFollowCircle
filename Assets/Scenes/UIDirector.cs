9eca9b912d9a914f00b1a6b13dc440ee0e4e4a52
seikuu2 <reikuu2@yahoo.co.jp>
2019-07-09 10:13:07 +0900

    UIベース
    ディレクターっぽい何かからスコアを受け取って描画します。

---
+++ Assets/Scenes/UIDirector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDirector : MonoBehaviour
+{
+    [SerializeField]
+    GameObject text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ディレクターかそのへんからスコアを取得
+        // 一時的に0代入
+        float score = 0;
+        // 反映
+        this.text.GetComponent<Text>().text =
+            "スコア：" + score.ToString("F1"); 
+    }
+}
