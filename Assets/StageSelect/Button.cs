69600fe256919bcb8f1e463caac130bbe392c76d
seikuu2 <reikuu2@yahoo.co.jp>
2019-07-29 02:12:23 +0900

    ステージ選択シーンができたよ！
    選択レベル変数あるからプレイシーンさんは受け皿変数作ってねって言っても読まないと思うから口頭で言います。

---
+++ Assets/StageSelect/Button.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+
+    bool  pushflag = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        pushflag = false;
+    }
+    
+    public bool GetFlug()
+    {
+        return pushflag;
+    }
+    public void SetFlug()
+    {
+        pushflag = true;
+    }
+}
